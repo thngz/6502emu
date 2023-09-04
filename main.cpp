c0cc3bdfce68c92f79494dac366d62ea0997e898
lollipas <draakonlohe123@gmail.com>
2023-09-04 22:41:36 +0300

    finished setting up googletest

--- main.cpp
+++ main.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
-#include "cpu.h"
+#include "gtest/gtest.h"
 
-int main() {
-    Memory mem;
-    CPU cpu(mem, 9);
-    cpu.reset();
+#if GTEST_OS_ESP8266 || GTEST_OS_ESP32
+#if GTEST_OS_ESP8266
+extern "C" {
+#endif
+	void setup() {
+		testing::InitGoogleTest();
+	}
 
-    mem[0xFFFC] = CPU::JSR;
-    mem[0xFFFD] = 0x42;
-    mem[0xFFFE] = 0x42;
-    mem[0x4242] = CPU::LDA_IM;
-    mem[0x4243] = 0x84;
-    cpu.execute();
-    return 0;
+	void loop() { RUN_ALL_TESTS(); }
+
+#if GTEST_OS_ESP8266
 }
+#endif
+
+#else
+
+GTEST_API_ int main( int argc, char **argv ) {
+    printf( "Running main() from %s\n", __FILE__ );
+    testing::InitGoogleTest( &argc, argv );
+    return RUN_ALL_TESTS();
+}
+#endif
+
+
--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,30 @@
 //
 // Created by Georg on 04.09.2023.
 //
-
 #include <gtest/gtest.h>
+#include "cpu.h"
+
+class Test1 : public testing::Test {
+public:
+
+    Memory mem;
+    CPU cpu {mem, 9};
+
+    void SetUp() override {
+        cpu.reset();
+
+    }
+};
+
+TEST_F(Test1, RunInlineProgram) {
+
+    cpu.reset();
+
+    mem[0xFFFC] = CPU::JSR;
+    mem[0xFFFD] = 0x42;
+    mem[0xFFFE] = 0x42;
+    mem[0x4242] = CPU::LDA_IM;
+    mem[0x4243] = 0x84;
+    cpu.execute();
+}
+
